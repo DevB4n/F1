4c186ca773b3e416729607df305a15f6e4a720e8
DevB4n <buenochaconesteban@gmail.com>
2025-04-29 13:41:51 -0500

    feat: :sparkles: nuevos features de registro y inicio de carga json

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+//boton de iniciar sesion
+
 const popUp = document.getElementById('pop-up')
 const signIn = document.getElementById('log-in-btn')
 const login = document.getElementById('login-button')
 
+// boton de registrar
+const registerPopUp = document.getElementById('register-pop-up')
+const registerBtn = document.getElementById('sign-in-btn')
+const registerConfirm = document.getElementById('register-button')
+
+//sign in boton
 
 signIn.addEventListener('click',()=>{
     popUp.style.opacity = 1;
     popUp.style.pointerEvents = 'auto';
     popUp.style.transform = 'scale(1)';
+    document.body.classList.add('blur-active');
 })
 
 login.addEventListener('click', () =>{
     popUp.style.opacity = 0;
     popUp.style.pointerEvents = 'none';
     popUp.style.transform = 'scale(0.8)';
+    document.body.classList.remove('blur-active');
 })
 
+//register boton
+
+registerBtn.addEventListener('click', () => {
+    registerPopUp.style.opacity = 1;
+    registerPopUp.style.pointerEvents = 'auto';
+    registerPopUp.style.transform = 'scale(1)';
+    document.body.classList.add('blur-active');
+})
+
+registerConfirm.addEventListener('click', () => {
+    registerPopUp.style.opacity = 0;
+    registerPopUp.style.pointerEvents = 'none';
+    registerPopUp.style.transform = 'scale(0.8)';
+    document.body.classList.remove('blur-active');
+})
